2a976ee7ed843846dad6ad474f480526b451f1f9
Heliton Bruning <heliton.bruning@gmail.com>
2020-10-06 17:47:31 -0300

    no message

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -51,28 +51,58 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
+  const token = getToken(authHeader)
+  const response = await Axios.get(jwksUrl);
+  //const jwks = response.data;
+  //const keys:any[] = jwks.keys;
+  
+  //logger.info('jwks', jwks)
+
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  //Find the signing key in the filtered JWKS with a matching kid property.  
+  const signingKey = response.data.keys.filter(key => key.kid === jwt.header.kid);
+  logger.info('signingKey', signingKey)
+  let certValue:string = signingKey.x5c[0];
+  
+  certValue = certValue.match(/.{1,64}/g).join('\n');
+  const finalCertKey:string = `-----BEGIN CERTIFICATE-----\n${certValue}\n-----END CERTIFICATE-----\n`;
+  //logger.info("finalCertKey - "+util.inspect(finalCertKey, false, null, true));
+  logger.info('finalCertKey', finalCertKey)
+ 
+  let jwtPayload:JwtPayload = verify(token, finalCertKey, { algorithms: ['RS256'] }) as JwtPayload; 
+  return jwtPayload;
+}
+
+/*async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader);
   const jwt: Jwt = decode(token, { complete: true }) as Jwt;
   const jwtKid = jwt.header.kid;
   let cert: string | Buffer;
 
+  console.info('token : ', token);
+
   try {
     const jwks = await Axios.get(jwksUrl);
     const signingKey = jwks.data.keys.filter(k => k.kid === jwtKid)[0];
 
+    logger.info('signingKey', signingKey)
+
     if (!signingKey) {
       throw new Error(`Unable to find a signing key that matches '${jwtKid}'`);
     }
+    
+    logger.info('signingKey.x5c[0]', signingKey.x5c[0])
     const { x5c } = signingKey;
 
     cert = `-----BEGIN CERTIFICATE-----\n${x5c[0]}\n-----END CERTIFICATE-----`;
-    console.log(cert );
+    console.info(cert );
   } catch (error) {
-    console.log('Error While getting Certificate : ', error);
+    console.info('Error While getting Certificate : ', error);
   }
 
-  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
-}
+  let jwtPayload:JwtPayload = verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload; 
+  return jwtPayload
+}*/
 
 function getToken(authHeader: string): string {
   if (!authHeader) throw new Error('No authentication header')
